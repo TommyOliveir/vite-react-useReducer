e86a0875bfa7f753610347d745f0c53343c0d607
TommyOliveir <90339122+TommyOliveir@users.noreply.github.com>
2023-05-19 16:17:33 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import CounterOne from './components/CounterOne'
+import React, { useState, useReducer } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import CounterOne from "./components/CounterOne";
+import CounterTwo from "./components/CounterTwo";
+import CounterThree from "./components/CounterThree";
+import reducer2 from "./utils/reducer2";
+import ComponentA from "./components/ComponentA";
+
+
+export const CountContext = React.createContext();
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return initialState;
+    default:
+      return state;
+  }
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, dispatch] = useReducer(reducer2, initialState);
 
   return (
-    <>
-      <h1>This is useReducer samples snippet</h1>
-      <CounterOne/>
-    </>
-  )
+    <CountContext.Provider value={{countState: count, countDispatch: dispatch }}>
+      <>
+        <h1>This is useReducer samples snippet</h1>
+        count from app js - {count}
+        <ComponentA />
+        {/* <CounterOne/> */}
+        {/* <CounterTwo/> */}
+        {/* <CounterThree/> */}
+      </>
+    </CountContext.Provider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/utils/reducer2.js
@@ -0,0 +1,14 @@
+ const reducer2 = (state, action) => {
+    switch (action) {
+      case "increment":
+        return state + 2;
+      case "decrement":
+        return state - 2;
+      case "reset":
+        return initialState;
+      default:
+        return state;
+    }
+  };
+
+export default reducer2
